328357b73184e6b831ebdff9cd31af025dcb39e3
lihongwei <lihongwei1239@outlook.com>
2017-06-04 15:17:06 +0800

    lihongwei

--- microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaAppliaction.java
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaAppliaction.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerAppliaction {
+public class EurekaAppliaction {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerAppliaction.class, args);
+		SpringApplication.run(EurekaAppliaction.class, args);
 	}
 }
